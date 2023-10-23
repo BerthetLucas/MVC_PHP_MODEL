a78faa93b0ba055df96903ba399e672150b41585
Lucas Berthet <lu.berthet@gmail.com>
2023-10-23 12:37:08 +0200

    remote access

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+//Declaration d'une constante 
+
+const CONFIG_SITE_TITLE = "MVC PHP 🐘";
+
+const CONFIG_ROUTES = [
+    "home" => "template_home",
+    "gallery" => "template_gallery",
+    "contact" => "template_contact",
+];
+
+const DB_NAME = "mabdd"; 
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+// Fichier de config
+require("./config.php"); 
+// Fichier routeur
+require("./router.php"); 
+// Chargement de la vue 
+include("./views/layout.phtml"); 
+
+?>
