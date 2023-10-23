daf866de645f743708672b8b7846ca6f2b6793c5
Lucas Berthet <lu.berthet@gmail.com>
2023-10-23 14:17:02 +0200

    fixe router

--- router.php
+++ router.php
@@ -1,8 +1,8 @@
 <?php
 
-if (isset($_GET['page'])) {
+if (isset($_GET['page']) && file_exists("./views/template_". $_GET['page'].".phtml") ) {
     $page = $_GET['page']; 
-    $template = CONFIG_ROUTES["$page"]; 
+} else {
+    $page='home'; 
 }
-
-?>
+$template = CONFIG_ROUTES[$page];
